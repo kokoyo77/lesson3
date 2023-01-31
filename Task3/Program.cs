087da0a8eece1305759be50553486bc792f18e6c
artem <normandysr777@yandex.ru>
2023-01-31 16:26:16 +0300

    Lesson3.1

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] number){
+  int counter = 0;
+  int length = number.Length;
+  while (counter <  length){
+    number[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArray(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] array = new int[number+1];
+Cube(array);
+PrintArray(array);
